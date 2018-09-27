acd7c20376258b070c24e224b7ca85dcb590a566
SubMishMar <subodheenitr@gmail.com>
2018-09-27 03:19:54 -0500

    updated plotfile

--- plot/skyviper_plot.py
+++ plot/skyviper_plot.py
@@ -16,6 +16,46 @@ with open('local_position.txt','r') as csvfile:
     	y_lp.append(float(column[5]))
     	z_lp.append(float(column[6]))
 
-plt.plot(z_lp);
+time_mp = []
+x_mp = []
+y_mp = []
+z_mp = []
+
+with open('mocap_pose.txt','r') as csvfile:
+    lines = csv.reader(csvfile, delimiter=',')
+    next(lines)
+    for column in lines:
+    	time_mp.append(float(column[0]))
+    	x_mp.append(float(column[4]))
+    	y_mp.append(float(column[5]))
+    	z_mp.append(float(column[6]))
+
+fig, axs = plt.subplots(3, 1)
+axs[0].plot(time_lp, x_lp);
+axs[0].hold(True)
+axs[0].plot(time_mp, x_mp);
+axs[0].hold(False)
+axs[0].legend(['EKF', 'Vision'])
+axs[0].set_ylabel('X')
+axs[0].grid()
+
+axs[1].plot(time_lp, y_lp);
+axs[1].hold(True)
+axs[1].plot(time_mp, y_mp);
+axs[1].hold(False)
+axs[1].legend(['EKF', 'Vision'])
+axs[1].set_ylabel('Y')
+axs[1].grid()
+
+axs[2].plot(time_lp, z_lp);
+axs[2].hold(True)
+axs[2].plot(time_mp, z_mp);
+axs[2].hold(False)
+axs[2].legend(['EKF', 'Vision'])
+axs[2].set_xlabel('time')
+axs[2].set_ylabel('Z')
+axs[2].grid()
+
 plt.show()
+
 
